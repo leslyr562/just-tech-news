0b47575dd1b299672f6a4241829e99f1adae278a
Lesly <leslyr562@gmail.com>
2021-11-16 03:09:37 -0600

    added and tested routes

--- models/index.js
+++ models/index.js
@@ -1,3 +1,12 @@
 const User = require('./user');
 const Post = require('./post')
 module.exports = { User, Post };
+
+// create associations
+User.hasMany(Post, {
+    foreignKey: 'user_id'
+});
+
+Post.belongsTo(User, {
+    foreignKey: 'user_id'
+});
--- server.js
+++ server.js
@@ -14,6 +14,7 @@ app.use(routes);
 //turn on connection to db and server 
 //"sync" part means that this is Sequelize taking the models and connecting them to associated database tables. If it doesn't find a table, it'll create it for you!
 //The other thing to notice is the use of {force: false} in the .sync() method. This doesn't have to be included, but if it were set to true, it would drop and re-create all of the database tables on startup.
+//if changed to true it drops database and restarts Module 13.3.5
 sequelize.sync({ force: false}).then(() => {
     app.listen(PORT, () => console.log('Now listening'));
 });
