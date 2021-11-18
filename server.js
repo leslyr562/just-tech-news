4306eb055541140472da4fbba7a6717a5b630a85
Lesly <leslyr562@gmail.com>
2021-11-18 02:27:03 -0600

    sign up sheet

--- server.js
+++ server.js
@@ -1,15 +1,28 @@
 const express = require('express');
-const routes = require('./routes');
-const sequelize = require('./config/connection');
+const path = require('path');
+const exphbs = require('express-handlebars');
+
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+const sequelize = require('./config/connection');
+
+const hbs = exphbs.create({});
+
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+
 app.use(express.json());
-app.use(express.urlencoded({ extended: true}));
+app.use(express.urlencoded({ extended: false}));
+//adding folder through express.static middleware gets connect and serves as static assets
+app.use(express.static(path.join(__dirname, 'public')));
+
 
 //turn on routes
-app.use(routes);
+app.use(require('./controllers'));
+
+
 
 //turn on connection to db and server 
 //"sync" part means that this is Sequelize taking the models and connecting them to associated database tables. If it doesn't find a table, it'll create it for you!
