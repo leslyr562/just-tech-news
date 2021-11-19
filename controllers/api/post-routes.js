4f3a578e391d1e26bed779539a9df5a5d2574ac0
Lesly <leslyr562@gmail.com>
2021-11-18 18:47:39 -0600

    login ifs

--- controllers/api/post-routes.js
+++ controllers/api/post-routes.js
@@ -1,5 +1,5 @@
 const router = require('express').Router();
-const { Post, User, Comment } = require('../../models');
+const { Post, User, Comment, Vote } = require('../../models');
 const sequelize = require('../../config/connection');
 
 
@@ -99,13 +99,25 @@ router.post('/', (req, res) => {
 router.put('/upvote', (req, res) => {
     // create the vote
     // custom static method created in models/Post.js
-    Post.upvote(req.body, { Vote })
-        .then(updatedPostData => res.json(updatedPostData))
-        .catch(err => {
+      // make sure the session exists first
+    if (req.session) {
+        // pass session id along with all destructured properties on req.body
+        Post.upvote({ ...req.body, user_id: req.session.user_id }, { Vote, Comment, User })
+          .then(updatedVoteData => res.json(updatedVoteData))
+          .catch(err => {
             console.log(err);
-            res.status(400).json(err);
-        });
-});
+            res.status(500).json(err);
+          });
+      }
+    });
+      //old code to test on insomnia
+    // Post.upvote(req.body, { Vote })
+    //     .then(updatedPostData => res.json(updatedPostData))
+    //     .catch(err => {
+    //         console.log(err);
+    //         res.status(400).json(err);
+
+
 
 
 //modify the title
