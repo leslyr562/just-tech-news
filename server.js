497773bff22031db344c2711f6395eeeed1792b4
Lesly <leslyr562@gmail.com>
2021-11-19 01:47:31 -0600

    done

--- server.js
+++ server.js
@@ -2,9 +2,11 @@ const path = require('path');
 const express = require('express');
 const session = require('express-session');
 const exphbs = require('express-handlebars');
+const helpers = require('./utils/helpers');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
+const hbs = exphbs.create({ helpers });
 
 const sequelize = require('./config/connection');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
@@ -24,7 +26,7 @@ const sess = {
 //This code connects the session to our Sequelize database.
 app.use(session(sess));
 
-const hbs = exphbs.create({});
+// const hbs = exphbs.create({});
 
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
